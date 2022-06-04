e30b862ac7b0e21966d9bccd434bba0df3ef8327
Matthew Laux <matthew.laux@gmail.com>
2022-06-03 19:43:48 -0500

    scrolling with mouse kinda

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-let theCanvas;
-let ctx;
+let theCanvas = null;
+let ctx = null;
 let gridSize = 32;
+
+let scrollOffsetX = 0;
+let scrollOffsetY = 0;
+let lastX = 0;
+let lastY = 0;
+let dragging = false;
 
 function init() {
   theCanvas = document.getElementById('the-canvas');
@@ -8,25 +14,51 @@ function init() {
   theCanvas.width = window.innerWidth;
   theCanvas.height = window.innerHeight - 32;
 
+  theCanvas.onmousedown = evt => {
+    lastX = evt.offsetX;
+    lastY = evt.offsetY;
+    dragging = true;
+  }
+  theCanvas.onmouseup = () => {
+    dragging = false;
+  }
+  theCanvas.onmousemove = evt => {
+    if (!dragging) {
+      return;
+    }
+
+    scrollOffsetX += evt.offsetX - lastX;
+    scrollOffsetY += evt.offsetY - lastY;
+
+    lastX = evt.offsetX;
+    lastY = evt.offsetY;
+
+    console.log(scrollOffsetX, scrollOffsetY);
+    refreshCanvas();
+  }
+
   ctx = theCanvas.getContext('2d');
   ctx.lineWidth = 2;
-  ctx.strokeStyle = '#eee';
+  ctx.strokeStyle = '#ddd';
 
   refreshCanvas();
 }
 
 function refreshCanvas() {
+  ctx.fillStyle = '#fff';
+  ctx.fillRect(0, 0, theCanvas.width, theCanvas.height);
+
   drawGrid();
 }
 
 function drawGrid() {
-  for (let y = 0; y < theCanvas.height; y += gridSize) {
+  for (let y = scrollOffsetY; y < theCanvas.height; y += gridSize) {
     ctx.beginPath();
     ctx.moveTo(0, y);
     ctx.lineTo(theCanvas.width, y);
     ctx.stroke();
   }
-  for (let x = 0; x < theCanvas.width; x += gridSize) {
+  for (let x = scrollOffsetX; x < theCanvas.width; x += gridSize) {
     ctx.beginPath();
     ctx.moveTo(x, 0);
     ctx.lineTo(x, theCanvas.height);
